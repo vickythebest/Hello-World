a64e00cf2e3e1079f024f319ac9f75e129e33291
user1 <vickythebest008@gmail.com>
2019-12-14 20:30:25 -0800

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @author vivek
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		for(int i=0;i<=10;i++) {
+			
+			System.out.println("Print Count: - "+i);
+		}
+	}
+}
